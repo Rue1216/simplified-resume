e852fde3e810d657ebdf29a1618d66d77491654e
Rue <ltsuyun1999@gmail.com>
2022-04-12 11:14:10 +0800

    adjust loading event

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
+import React, { Suspense } from 'react';
 import About from "./components/About";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
-import Spline from "./components/Spline";
 import Timeline from "./components/Timeline";
-import { AnimatePresence } from 'framer-motion'
-import { useState,useEffect } from "react";
-import { PuffLoader } from "react-spinners";
+import { AnimatePresence, LazyMotion, domAnimation, m } from 'framer-motion';
+
+const SplineScene = React.lazy(()=>import("./components/Spline"));
 
 function App() {
-  const [loading, setloading] = useState(false);
-  
-  useEffect(()=>{
-    setloading(true)
-    setTimeout(() => {
-      setloading(false)
-    }, 3000);
-  },[])
 
   return (
     <AnimatePresence initial={false}>
-      {
-        loading?
-
-        <div className="w-full min-h-screen bg-gray-600 flex justify-center items-center align-middle">
-          <PuffLoader 
-            size={150}
-            color={"#F59A00"}
-            loading={loading}
-            speedMultiplier={1.5}
-          />
-        </div>
-        :
         <div className="wrapper">
-          <Navbar/>
-          <Spline/>
-          <About/>
-          <Timeline/>
-          <Projects/>
-          <Footer/>
+          <Suspense
+            fallback={
+              // spinner
+              <div className='w-full min-h-screen flex items-center justify-center'>
+                <div className='animate-ping bg-orange-400 w-10 h-10 rounded-full'></div>
+              </div>
+            }
+          >
+            <Navbar/>
+            <LazyMotion features={domAnimation}>
+              <m.div
+                initial={{opacity:0}}
+                animate={{opacity:1}}
+                transition={{duration:1}}
+              >
+                <SplineScene/>
+              </m.div>
+            </LazyMotion>
+            <About/>
+            <Timeline/>
+            <Projects/>
+            <Footer/>
+          </Suspense>
         </div>
-      }
     </AnimatePresence>
     
   );
--- src/components/Spline.jsx
+++ src/components/Spline.jsx
@@ -1,20 +1,14 @@
 import Spline from "@splinetool/react-spline";
 import React from 'react';
-import { LazyMotion, domAnimation, m } from "framer-motion"
 
 function SplineComponent() {
     return ( 
-        <LazyMotion features={domAnimation}>
-            <m.section
-                initial={{opacity:0}}
-                animate={{opacity:1}}
-                
-                transition={{duration:1}}
-                className='flex flex-col justify-center items-center relative' id='home'>
-                <Spline scene="https://draft.spline.design/qsaQMLk5bn5B-K9N/scene.spline" />
-                <div className="motion-hint">Press & Drag to Orbit</div>
-            </m.section>
-        </LazyMotion>
+        <section
+            className='flex flex-col justify-center items-center relative' id='home'>
+            <Spline scene="https://draft.spline.design/qsaQMLk5bn5B-K9N/scene.spline" />
+            <div className="motion-hint">Press & Drag to Orbit</div>
+        </section>
+       
         
      );
 }
